8765eaa10c0b1c6dbb7890bc2308901814b795b9
Ravandhika <ravandhika123@gmail.com>
2019-09-25 15:50:01 +0700

    Praktikum static activity

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/StaticActivitity.java
@@ -0,0 +1,17 @@
+package polinema.ac.id.starterchapter05.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import polinema.ac.id.starterchapter05.R;
+
+public class StaticActivitity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_static_activitity);
+
+    }
+}
