4947308730e3ec96d0695618cd7dca7889ae2dda
Ravandhika <ravandhika123@gmail.com>
2019-10-01 17:41:24 +0700

    Fragment Dips dan Handstand tugas Fragment

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/Fitness.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/Fitness.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 
 import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragments.DipsFragment;
+import polinema.ac.id.starterchapter05.fragments.HandstandFragment;
 import polinema.ac.id.starterchapter05.fragments.PushupFragment;
 
 public class Fitness extends AppCompatActivity {
@@ -19,9 +21,25 @@ public class Fitness extends AppCompatActivity {
 
     public void handleClickPushupFragment(View view) {
         FragmentTransaction pushupFragment = getSupportFragmentManager().beginTransaction();
-        pushupFragment.replace(R.id.pushups_fragment, new PushupFragment());
+        pushupFragment.replace(R.id.Fragment_dynamic, new PushupFragment());
         pushupFragment.addToBackStack(null);
         pushupFragment.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_from_right);
         pushupFragment.commit();
+    }
+
+    public void handleClickHandstandFragment(View view) {
+        FragmentTransaction handstandFragment = getSupportFragmentManager().beginTransaction();
+        handstandFragment.replace(R.id.Fragment_dynamic, new HandstandFragment());
+        handstandFragment.addToBackStack(null);
+        handstandFragment.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_from_right);
+        handstandFragment.commit();
+    }
+
+    public void handleClickDipsFragment(View view) {
+        FragmentTransaction DipsFragment = getSupportFragmentManager().beginTransaction();
+        DipsFragment.replace(R.id.Fragment_dynamic, new DipsFragment());
+        DipsFragment.addToBackStack(null);
+        DipsFragment.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_from_right);
+        DipsFragment.commit();
     }
 }
